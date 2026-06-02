7352bf650df3afb495d9cd5e3ece4d3e2341767d
Rohit Kharkar <rohitkharkar692@gmail.com>
2026-06-02 13:01:08 +0530

    Initial commit

---
+++ movies.js
@@ -0,0 +1,93 @@
+const baseurl = "https://api.themoviedb.org/3"
+const api = "c2b2450a7d2e59a0d4e951029f99dd83"
+const url ="https://api.themoviedb.org/3/movie/popular?api_key=";
+const next = document.querySelector('#next')
+
+let pages = 1
+
+next.addEventListener('click',()=>{
+     pages++;
+    window.history.pushState({}, "", `?page=${pages}`); 
+    getMovies();
+     window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+    
+})
+
+prev.addEventListener("click", () => {
+  if (pages > 1) {
+    pages--;
+    window.history.pushState({}, "", `?page=${pages}`);
+
+    getMovies();
+
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  }
+});
+
+
+
+
+let popularMovies = []
+
+async function getMovies(){
+    try{
+        const response = await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=c2b2450a7d2e59a0d4e951029f99dd83&page=${pages}`);
+        const data =  await response.json();
+        popularMovies =  data.results.slice(0,20)
+        renderUi()
+        
+    }
+    catch(error){
+        console.log(error);
+        
+    }
+}
+
+
+getMovies()
+
+function renderUi(){
+    console.log(popularMovies);
+    
+    const  movieContainer = document.querySelector('.movie-container');
+     movieContainer.innerHTML = "";
+    popularMovies.forEach(movie =>{
+
+        const card = document.createElement('div')
+        card.classList.add('card');
+
+
+        card.innerHTML= `
+         <img src="https://image.tmdb.org/t/p/w500/${movie.poster_path}" alt="moviePoster">
+                    <div class="card-info">
+                        <p>${movie.title}</p>
+                        <div class="rating">
+                            <svg xmlns="http://w3.org" viewBox="0 0 24 24" width="20" height="20">
+                                <path
+                                    d="M12 17.27L18.18 21l-1.64-7.03L22 9.24l-7.19-.61L12 2 9.19 8.63 2 9.24l5.46 4.73L5.82 21z"
+                                    fill="#FFD700" />
+                            </svg>
+                            <span id="rate">${movie.vote_average}</span>
+                            <span>|</span>
+                            <span id="year">${movie.release_date}</span>
+
+                        </div>
+                        <div class="about">Science Fiction, Adventure, Fantasy</div>
+         </div>
+        
+        `
+movieContainer.appendChild(card)
+
+    })
+
+}
+
+
+
+
---
+++ script.js
@@ -0,0 +1,202 @@
+const url ="https://api.themoviedb.org/3/trending/movie/day?api_key=c2b2450a7d2e59a0d4e951029f99dd83";
+const imageUrl = "https://image.tmdb.org/t/p/original";
+
+let topMovies = [];
+let topRated = [];
+let popMoviesa = [];
+
+async function getMovie() {
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+
+    topMovies = data.results.slice(0, 20);
+
+    renderUI();     
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+getMovie();
+
+
+function renderUI() {
+  console.log(topMovies);
+  
+  const heroSlider = document.querySelector(".hero-slider");  
+  heroSlider.innerHTML = `
+      <button class="btn prev">&#10094;</button>
+      <button class="btn next">&#10095;</button>
+  `
+
+  topMovies.forEach((movie, index) => {
+    getTrailer(movie.id); //
+
+    const slide = document.createElement("div");
+    slide.classList.add("slide");
+
+    if (index === 0) {
+      slide.classList.add("active");
+    }
+
+    slide.style.backgroundImage =
+      `url(${imageUrl}${movie.backdrop_path})`;
+
+    slide.innerHTML = `
+    
+    <div class="movie-title">
+        <p id="trending">Trending Now 🔥</p>
+        <h1 id="title">${movie.title}</h1>
+        <ul class="years">
+            <li class="star"><svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" viewBox="0 0 24 24"
+                    fill="#fbff00" stroke="#fbff00" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+                    class="lucide lucide-star-icon lucide-star">
+                    <path
+                        d="M11.525 2.295a.53.53 0 0 1 .95 0l2.31 4.679a2.123 2.123 0 0 0 1.595 1.16l5.166.756a.53.53 0 0 1 .294.904l-3.736 3.638a2.123 2.123 0 0 0-.611 1.878l.882 5.14a.53.53 0 0 1-.771.56l-4.618-2.428a2.122 2.122 0 0 0-1.973 0L6.396 21.01a.53.53 0 0 1-.77-.56l.881-5.139a2.122 2.122 0 0 0-.611-1.879L2.16 9.795a.53.53 0 0 1 .294-.906l5.165-.755a2.122 2.122 0 0 0 1.597-1.16z" />
+                </svg><span id="rateing">${movie.vote_average}</span></li>
+            <li>|</li>
+            <li id="year">${movie.release_date}</li>
+            <li>|</li>
+            <li id="hours">2h 32m</li>
+        </ul>
+        <p id="movie-disc">${movie.overview}</p>
+        <div class="buttons">
+            <button id="trailer">
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
+                    stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+                    class="lucide lucide-play-icon lucide-play">
+                    <path d="M5 5a2 2 0 0 1 3.008-1.728l11.997 6.998a2 2 0 0 1 .003 3.458l-12 7A2 2 0 0 1 5 19z" />
+                </svg>
+                Watch Trailer
+            </button>
+            <button id="mylist">
+                <p style="font-size: 32px;">+</p>
+                <p>My List</p>
+            </button>
+        </div>
+    </div> 
+    `;
+
+    
+    heroSlider.appendChild(slide);
+  });
+
+
+
+   const card_list = document.querySelector('.card_list');
+
+    topMovies.forEach((movie, index) => {
+
+      const card_itam = document.createElement('div')
+      card_itam.classList.add('card_itam')
+
+
+      card_itam.innerHTML = `
+      <img src="https://image.tmdb.org/t/p/original${movie.poster_path}" id="card-image">
+                        <div class="movie-detail">
+                            <h4 id="movie_name">${movie.title}</h4>
+                            <div class="below">
+                                <p id="movie-rating">⭐${movie.vote_average}</p>
+                                <p class="Realseyear">${movie.release_date}</p>
+                            </div>
+                        </div>
+      `
+    card_list.appendChild(card_itam)  
+    })
+
+  startSlider();
+
+}
+
+
+async function getTrailer(movieId) {
+
+  try {
+
+    const response = await fetch(
+      `https://api.themoviedb.org/3/movie/${movieId}/videos?api_key=c2b2450a7d2e59a0d4e951029f99dd83`
+    );
+
+    const data = await response.json();
+   
+
+    let selectedVideo = data.results.find(
+      video =>
+        video.type === "Trailer" &&
+        video.site === "YouTube"
+    );
+
+
+
+    if (!selectedVideo) {
+
+      selectedVideo = data.results.find(
+        video =>
+          video.type === "Teaser" &&
+          video.site === "YouTube"
+      );
+
+    }
+
+    if (selectedVideo) {
+
+
+      const youtubeUrl =
+        `https://www.youtube.com/watch?v=${selectedVideo.key}`;
+
+
+
+    } else {
+
+      console.log("No Trailer or Teaser Found");
+
+    }
+
+  } catch (error) {
+
+    console.log(error);
+
+  }
+}
+
+
+
+
+
+
+function startSlider() {
+  const slides = document.querySelectorAll(".slide");
+  const nextBtn = document.querySelector(".next");
+  const prevBtn = document.querySelector(".prev");
+
+  let current = 0;
+
+  function showSlide(index) {
+    slides.forEach((slide) =>
+      slide.classList.remove("active")
+    );
+
+    slides[index].classList.add("active");
+  }
+
+  function nextSlide() {
+    current = (current + 1) % slides.length;
+    showSlide(current);
+  }
+
+  function prevSlide() {
+    current = (current - 1 + slides.length) % slides.length;
+    showSlide(current);
+  }
+
+  nextBtn.addEventListener("click", nextSlide);
+  prevBtn.addEventListener("click", prevSlide);
+
+  setInterval(nextSlide, 5000);
+}
+
+
+
+
+
